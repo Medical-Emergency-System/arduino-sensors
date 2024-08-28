7cbc8e7df4f9b03c5dee9534c01c2ce85c82958e
MHD Yasser Haddad <yasserhaddad99@gmail.com>
2024-08-28 13:43:09 +0200

    Initial commit

---
+++ code/code.ino
@@ -0,0 +1,11 @@
+int outputPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(outputPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(outputPin, 1);
+}
